31982addc75926c97165a9a68ebeab1096247209
Matheus <matheuscristo.f@hotmail.com>
2026-09-22 03:16:24 -0300

    Models Categoria, Conta e Lançamento com admin registrado

--- financas/admin.py
+++ financas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Conta, Lnacamento
 
-# Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Conta)
+admin.site.register(Lnacamento)
+
